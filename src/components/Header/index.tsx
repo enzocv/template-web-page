5db35ef48281a81c95c00af42f00efd9a69a06af
Enzo Catalan <enzocv96@gmail.com>
2024-10-22 16:55:15 -0500

    se remueve clase container de header nav

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from 'react';
-import Nav from '../Nav';
-import useWindowSize from '../../hooks/useWindowSize';
+import React, { useEffect, useState } from "react";
+import Nav from "../Nav";
+import useWindowSize from "../../hooks/useWindowSize";
 
 const Header: React.FC = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -14,17 +14,19 @@ const Header: React.FC = () => {
     if (width > 767 && isOpen) toggleMenu();
   }, [width, isOpen]);
 
-
   return (
     <header className="bg-gray-800 text-white p-4 fixed w-full top-0 z-20">
-      <div className="container mx-auto flex justify-between items-center">
+      {/* <div className="container mx-auto flex justify-between items-center"> */}
+      <div className="flex justify-between items-center">
         <h1 className="text-xl font-bold">Mi Página</h1>
         <div className="md:flex md:items-center">
           <Nav isOpen={isOpen} />
           <div className="md:hidden">
             <button onClick={toggleMenu} className="focus:outline-none">
               <svg
-                className={`w-6 h-6 transition-transform transform ${isOpen ? 'rotate-45' : ''}`}
+                className={`w-6 h-6 transition-transform transform ${
+                  isOpen ? "rotate-45" : ""
+                }`}
                 fill="none"
                 stroke="currentColor"
                 viewBox="0 0 24 24"
@@ -34,7 +36,9 @@ const Header: React.FC = () => {
                   strokeLinecap="round"
                   strokeLinejoin="round"
                   strokeWidth="2"
-                  d={isOpen ? 'M6 18L18 6M6 6l12 12' : 'M4 6h16M4 12h16M4 18h16'}
+                  d={
+                    isOpen ? "M6 18L18 6M6 6l12 12" : "M4 6h16M4 12h16M4 18h16"
+                  }
                 />
               </svg>
             </button>
--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import './NavStyles.css';
-import NavItem from '../NavItem';
+import React from "react";
+import "./NavStyles.css";
+import NavItem from "../NavItem";
 
 interface NavProps {
   isOpen: boolean;
@@ -9,31 +9,37 @@ interface NavProps {
 const dataNavegacion = [
   {
     id: 1,
-    title: 'Inicio',
-    path: '/'
+    title: "Inicio",
+    path: "/",
   },
   {
     id: 2,
-    title: 'Nosotros',
-    path: '/notros'
+    title: "Nosotros",
+    path: "/notros",
   },
   {
     id: 3,
-    title: 'Contacto',
-    path: '/contacto'
+    title: "Contacto",
+    path: "/contacto",
   },
-]
+];
 
 const Nav: React.FC<NavProps> = ({ isOpen }) => {
   return (
     <nav
-      className={`fixed right-0 w-full h-screen bg-gray-800 text-white transform transition-transform duration-300 ease-in-out top-[62px] ${isOpen ? 'translate-x-0' : 'translate-x-full'
-        } md:relative md:translate-x-0 md:flex md:items-center md:bg-transparent md:h-auto`}
+      className={`fixed right-0 w-full h-screen bg-gray-800 text-white transform transition-transform duration-300 ease-in-out top-[62px] md:top-0 ${
+        isOpen ? "translate-x-0" : "translate-x-full"
+      } md:relative md:translate-x-0 md:flex md:items-center md:bg-transparent md:h-auto`}
       style={{ zIndex: 10 }}
     >
       <ul className="flex flex-col items-center h-full md:flex-row sm:nav-ul">
-        {dataNavegacion.map(item => (
-          <NavItem key={item.id} to={item.path} label={item.title} isOpen={isOpen} />
+        {dataNavegacion.map((item) => (
+          <NavItem
+            key={item.id}
+            to={item.path}
+            label={item.title}
+            isOpen={isOpen}
+          />
         ))}
       </ul>
     </nav>
